0f79178545319d8c52b24f2bbadb87adf56706cc
Sreehari S <44937241+sreeharisangeeth@users.noreply.github.com>
2021-06-18 22:32:02 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,82 @@
-/* Enter your solutions in this file */
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
+
+int max(int array[], int n)
+{
+    int m=array[0];
+    for(int i=1;i<n;i++)
+    {
+        if(array[i]>m)
+        {
+            m=array[i];
+        }
+    }
+    return(m);
+}
+
+int min(int array[], int n)
+{
+    int m=array[0];
+    for(int i=1;i<n;i++)
+    {
+        if(array[i]<m)
+        {
+            m=array[i];
+        }
+    }
+    return(m);
+}
+
+float average(int array[],int n)
+{
+    float avg,sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=array[i];
+    }
+    avg=sum/n;
+    return avg;
+}
+
+int mode(int array[], int n)
+{
+    int count,maxCount=0,i,j,maxVal;
+    for(i=0;i<n;i++)
+    {
+        count=0;
+        for(j=i;j<n;j++)
+        {
+            if(array[i]==array[j])
+            {
+                count+=1;
+            }
+        }
+        if(count>maxCount)
+        {
+            maxCount=count;
+            maxVal=array[i];
+        }
+    }
+    return(maxVal);
+}
+
+int factors(int n, int array[])
+{
+    int i=2,j=0;
+    while(i<=sqrt(n))
+    {
+        while(n%i==0)
+        {
+            n=n/i;
+            array[j]=i;
+            j++;
+        }
+        i++;
+    }
+    if(n!=1)
+    {
+        array[j]=n;
+        j++;
+    }
+    return(j);
+}
